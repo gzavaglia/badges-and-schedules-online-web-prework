92393611c544bf67ad8575eb49ea5444b8d89d13
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-26 20:59:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,9 +6,10 @@ end
 
 def batch_badge_creator(array)
   #speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  array.each do |name|
-  phrase = "Hello, my name is #{name}."
-  return phrase
+  speakers = []
+  phrase = "Hello, my name is #{element}."
+  array.each{|element| speakers << phrase}
+  return speakers
   end 
 end 
     
